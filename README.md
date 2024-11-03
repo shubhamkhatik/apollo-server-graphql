ef99fe64b88a5de68edb9e5a22e75cc5e03bca65
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2024-11-03 17:24:42 +0530

    query and mutation

---
+++ README.md
@@ -0,0 +1,21 @@
+# GraphQL Server Example
+
+A simple GraphQL server example using Apollo Server and TypeScript.
+
+
+## step by step implementation guide
+
+> selection of graphQL library
+
+https://graphql.org/community/tools-and-libraries/
+
+> craete apollo server
+
+https://www.apollographql.com/docs/apollo-server/getting-started
+
+## Getting Started
+
+To run the server, navigate to the project directory and execute the following command:
+
+```bash
+npm start
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,83 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
+export const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
+  
+  
   type Book {
+    id: String
     title: String
-    author: String
+    authorId: String
+    publishedYear: Int
+    author: Author
+  }
+  type Author {
+    id: String
+    name: String
+    bookIds: [String]
+    books: [Book]
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
+  
+  
   type Query {
     books: [Book]
+    authors: [Author]
   }
+    type Mutation {
+        addBook( title: String, publishedYear: Int, authorId: String): Book!
+    }
 `;
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-  // Resolvers define how to fetch the types defined in your schema.
+// =========================================================================
+ // A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const data = {
+  authors: [
+    { id: "1", name: "shubham khatik", bookIds: ["101", "102"] },
+    { id: "2", name: "Akshay gupta", bookIds: ["103"] },
+  ],
+  books: [
+    { id: "101", title: "System Design", publishedYear: 2000, authorId: "1" },
+    { id: "102", title: "frontend", publishedYear: 2010, authorId: "1" },
+    { id: "103", title: "ramayana", publishedYear: 2020, authorId: "2" },
+  ],
+};
+// Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-      books: () => books,
+export const resolvers = {
+  Book: {
+    author: (parent, args, context, info) => {
+      return data.authors.find((author) => author.id === parent.authorId);
     },
-  };
-  // The ApolloServer constructor requires two parameters: your schema
+  },
+  Author: {
+    books: (parent, args, context, info) => {
+      return data.books.filter((book) => book.authorId === parent.id);
+    },
+  },
+  Query: {
+    books: () => data.books,
+    authors: () => data.authors,
+  },
+  Mutation: {
+    addBook: (parent, args, context, info) => {
+        console.log(args);
+        const newBook = {...args, id: data.books.length + 1};
+        data.books.push(newBook)
+        return newBook;
+    }
+}
+};
+// ===============================================================================
+// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
